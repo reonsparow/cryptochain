323e341217e365932d9849ec66a481e92bbd63b7
reonsparow <reonsparow@gmail.com>
2021-05-24 14:02:07 +0600

    transaction added (input)

--- wallet/index.js
+++ wallet/index.js
@@ -1,14 +1,19 @@
 const { STARTING_BALANCE } = require("../config");
+const {ec} = require('../util');
+const cryptoHash = require('../util/crypto-hash')
 
 class Wallet{
 constructor (){
    this.balance= STARTING_BALANCE;
-}
 
-addWallet({balance, publicKey}){
-return new Wallet({balance, publicKey});
-}
+   this.keyPair = ec.genKeyPair();
 
-}
+   this.publicKey = this.keyPair.getPublic().encode('hex')};
+
+   sign(data){
+      return this.keyPair.sign(cryptoHash(data));
+   }
+
+};
 
 module.exports = Wallet;
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const {verifySignature} = require('../util');
 
 describe('Wallet', ()=>{
     let wallet;
@@ -11,7 +12,35 @@ describe('Wallet', ()=>{
         expect(wallet).toHaveProperty('balance');
     });
 
-    it ('has a `balance`', ()=>{
+    it ('has a `publicKey`', ()=>{
         expect(wallet).toHaveProperty('publicKey');
     });
+
+    describe('signing data',()=>{
+
+        const data = 'foobar';
+
+        it('verifies a signatur',()=>{
+
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(true);
+          
+        });
+
+        it('does not verify an invalid signature', ()=>{
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                })
+            ).toBe(false);
+        });
+    });
+
 });
